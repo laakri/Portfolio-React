03de4aa3ccfc63e5b76e8dbc3b5c1cfd040b583c
SEIFEDDINE JLASSI <68915309+laakri@users.noreply.github.com>
2025-08-18 11:24:52 +0100

    adding the tab names

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import About from "./Pages/About";
 import Projects from "./Pages/Projects";
 import ProjectPage from "./Pages/ProjectPage";
@@ -6,6 +7,19 @@ import DraftBoard from "./Pages/DraftBoard";
 
 // Accept handleImageLoaded as a prop
 const Routing = ({ handleImageLoaded }: { handleImageLoaded: () => void }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname.toLowerCase();
+    let section = "Home";
+    if (path.startsWith("/projects/")) section = "Project";
+    else if (path === "/projects") section = "Projects";
+    else if (path === "/draftboard") section = "Draft Board";
+    else if (path === "/") section = "Home";
+
+    document.title = `Seif Jlassi | ${section}`;
+  }, [location.pathname]);
+
   return (
     <Routes>
       <Route path="/" element={<About handleImageLoaded={handleImageLoaded} />} />
